22bcf2b0d4c03ed8996d200868a695a5fdf933b6
Arnab Pratihar <apratihar18@gmail.com>
2023-04-27 07:12:39 +0530

    Changecase updated

--- Solutions/ChangeCase.java
+++ Solutions/ChangeCase.java
@@ -3,25 +3,23 @@ import java.util.Scanner;
 public class ChangeCase {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.print("Enter a line of text: ");
         String line = input.nextLine();
         input.close();
-
-        String newLine = "";
-
-        for (int i = 0; i < line.length(); i++) {
-            char c = line.charAt(i);
-
-            if (Character.isUpperCase(c)) {
-                newLine += Character.toLowerCase(c);
-            } else if (Character.isLowerCase(c)) {
-                newLine += Character.toUpperCase(c);
-            } else {
-                newLine += c;
-            }
-        }
-
-        System.out.println("Changed case: " + newLine);
+        System.out.println(Upper_to_lower(line));
+        input.close();
+       
     }
+    static String Upper_to_lower(String str){
+        char[]arr=str.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+           if(Character.isUpperCase(arr[i])){
+              arr[i]=Character.toLowerCase(arr[i]);
+           }
+           else{
+            arr[i]=Character.toUpperCase(arr[i]);
+           }
+        }
+        return new String(arr);
+     }    
 }
